1baa95285eac201df00d35e16967b0a5e6405228
Ziehodou <cziehodou@gmail.com>
2023-06-07 13:40:49 +0100

    Code nettoyé

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,28 +5,32 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int pupilCount = 0;		
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
+		int i = 0;	
 		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
+		while (line != null) 
+		{
+			i++;
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+			if (line.equals("headache"))
+				{
+					headCount++;
+					System.out.println("number of headaches: " + headCount);
+				}
+			else if (line.equals("rush")) 
+			{
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
+			else if (line.contains("pupils")) 
+			{
 				pupilCount++;
 			}
 
